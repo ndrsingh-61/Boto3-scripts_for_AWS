c6a96a0dc0c7e10f0b3f3e11bb61034b4e1de2d4
ndr.singh <ndrsingh487@gmail.com>
2022-01-28 00:15:43 +0530

    added ec2 stop and start

--- ec2_instance.py
+++ ec2_instance.py
@@ -11,6 +11,32 @@ def create_ec2_instace():
                 KeyName         = "ec2-key"
         )
     except Exception as e:
-        print(e)    
+        print(e)   
 
-create_ec2_instace()
+def describe_ec2_instance():
+    try:
+        ec2_instance = boto3.client('ec2')
+        return str(ec2_instance.describe_instances()["Reservations"][0]["Instances"][0]["InstanceId"])
+    except Exception as e:
+        print(e)
+
+
+def stop_ec2_instance():
+    try:
+        ec2_instance = boto3.client('ec2')
+        instance_id = describe_ec2_instance()
+        ec2_instance.stop_instances(InstanceIds =[instance_id])
+    except Exception as e:
+        print(e)  
+
+def start_ec2_instance():
+    try:
+        ec2_instance = boto3.client('ec2')
+        instance_id = describe_ec2_instance()
+        ec2_instance.start_instances(InstanceIds = [instance_id])
+    except Exception as e:
+        print(e)    
+# create_ec2_instace()
+# describe_ec2_instance()
+# stop_ec2_instance()
+start_ec2_instance()
