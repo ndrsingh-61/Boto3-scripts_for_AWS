09885ea95fc49d1f1b9266d58d2e15072a5b0734
ndr.singh <ndrsingh487@gmail.com>
2022-02-02 23:54:40 +0530

    listing ec2 instances with detailed information

---
+++ list_all_the_ec2_instances.py
@@ -0,0 +1,12 @@
+import boto3
+from pprint import pprint
+aws_mng_con = boto3.session.Session()
+aws_ec2_inst = aws_mng_con.resource(service_name = 'ec2', region_name = 'us-east-1')
+cnt = 1
+for each in aws_ec2_inst.instances.all():
+    print(cnt, each, each.instance_id, each.instance_type, each.launch_time.strftime("%y-%m-%d"), each.private_ip_address)
+    cnt += 1
+
+# response = aws_ec2_inst.describe_instances()
+# pprint(response)
+
